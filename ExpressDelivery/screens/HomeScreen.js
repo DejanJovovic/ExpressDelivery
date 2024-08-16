592a58261b47e294a208ea5967d1c4a02e4d134a
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-08-16 13:05:45 +0200

    categories screen started with categories and categoryCard components

--- ExpressDelivery/screens/HomeScreen.js
+++ ExpressDelivery/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput, ScrollView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -6,9 +6,10 @@ import colors from "../config/colors";
 import {
   ChevronDownIcon,
   UserIcon,
-  SearchIcon,
-  AdjustmentIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -40,6 +41,30 @@ const HomeScreen = () => {
         </View>
         <UserIcon size={35} color={colors.primary} />
       </View>
+
+      {/* Search */}
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color={colors.primary} />
+      </View>
+
+      {/* Body */}
+
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
