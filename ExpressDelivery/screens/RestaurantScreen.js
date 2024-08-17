edf8a45b89445f80d2d14b3a8b32e367f11d9098
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-08-17 19:34:34 +0200

    started building the restaurant screen

--- ExpressDelivery/screens/RestaurantScreen.js
+++ ExpressDelivery/screens/RestaurantScreen.js
@@ -1,11 +1,51 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import { ArrowLeftIcon } from "react-native-heroicons/solid";
+import colors from "../config/colors";
 
 const RestaurantScreen = () => {
+  const navigation = useNavigation();
+
+  const {
+    params: {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      long,
+      lat,
+    },
+  } = useRoute();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>RestaurantScreen</Text>
-    </View>
+    <ScrollView>
+      <View className="relative">
+        <Image
+          source={{
+            uri: urlFor(imgUrl).url(),
+          }}
+          className="w-full h-56 bg-gray-300 p-4"
+        />
+        <TouchableOpacity
+          onPress={navigation.goBack}
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full "
+        >
+          <ArrowLeftIcon size={20} color={colors.primary} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
