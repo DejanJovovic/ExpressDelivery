957bc5530303cc69db01d3abf5ea00e0d03f60f8
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-08-19 12:40:35 +0200

    added the basket view that shows how many items are in the basket and how much they cost

--- ExpressDelivery/components/DishRow.js
+++ ExpressDelivery/components/DishRow.js
@@ -58,8 +58,14 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity onPress={removeItemFromBasket}>
-              <MinusCircleIcon color={colors.primary} size={40} />
+            <TouchableOpacity
+              disabled={!items.length}
+              onPress={removeItemFromBasket}
+            >
+              <MinusCircleIcon
+                color={items.length > 0 ? colors.primary : "gray"}
+                size={40}
+              />
             </TouchableOpacity>
 
             <Text>{items.length}</Text>
